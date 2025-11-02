f79c45bc5cff597a1b63f3532f1b49fab9f4309c
jullazarovych <juliala125@gmail.com>
2025-11-02 20:35:31 +0200

    1 task

---
+++ lab5/main.tf
@@ -0,0 +1,56 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "az104-rg5"
+  location = var.location 
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "CoreServicesVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "CoreSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "coreservicesvm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = "CoreServicesVM"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_D2s_v3"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  zone                = null 
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS" 
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
