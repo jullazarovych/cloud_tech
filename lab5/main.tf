dea435239715316280062cf30794e7141bd2b3d2
jullazarovych <juliala125@gmail.com>
2025-11-02 20:58:30 +0200

    task 2-3

--- lab5/main.tf
+++ lab5/main.tf
@@ -54,3 +54,52 @@ resource "azurerm_windows_virtual_machine" "vm" {
     version   = "latest"
   }
 }
+
+resource "azurerm_virtual_network" "vnet_mfg" {
+  name                = "ManufacturingVnet"
+  address_space       = ["172.16.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet_mfg" {
+  name                 = "ManufacturingSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet_mfg.name
+  address_prefixes     = ["172.16.0.0/24"]
+}
+
+resource "azurerm_network_interface" "nic_mfg" {
+  name                = "manufacturingvm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet_mfg.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "vm_mfg" {
+  name                = "ManufacturingVM"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_D2s_v3" 
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  zone                = null 
+  network_interface_ids = [
+    azurerm_network_interface.nic_mfg.id,
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS" 
+  }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
