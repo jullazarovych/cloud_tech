0a863308c1d16cd1f7aba6b4b03e7276890c41ba
jullazarovych <juliala125@gmail.com>
2025-11-23 22:21:53 +0200

    task 4

--- lab8/main.tf
+++ lab8/main.tf
@@ -119,3 +119,58 @@ resource "azurerm_windows_virtual_machine_scale_set" "vmss" {
     storage_account_uri = null
   }
 }
+resource "azurerm_monitor_autoscale_setting" "vmss_autoscale" {
+  name                = "vmss-autoscale"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  target_resource_id  = azurerm_windows_virtual_machine_scale_set.vmss.id
+
+  profile {
+    name = "defaultProfile"
+
+    capacity {  
+      default = "2"
+      minimum = "2"  
+      maximum = "10" 
+    }
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_windows_virtual_machine_scale_set.vmss.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT10M" 
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 70
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "PercentChangeCount"
+        value     = "50" 
+        cooldown  = "PT5M" 
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_windows_virtual_machine_scale_set.vmss.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT10M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 30
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "PercentChangeCount"
+        value     = "50" 
+        cooldown  = "PT5M"
+      }
+    }
+  }
+}
