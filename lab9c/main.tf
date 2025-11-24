3f3dab80f3443970d5b6733bf77a996a8e128313
jullazarovych <juliala125@gmail.com>
2025-11-24 17:42:07 +0200

    lab9c

---
+++ lab9c/main.tf
@@ -0,0 +1,44 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "az104-rg9c"
+  location = "East US" 
+}
+
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "juliala125-logs"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_container_app_environment" "env" {
+  name                       = "my-environment"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+}
+
+resource "azurerm_container_app" "app" {
+  name                         = "my-app"
+  container_app_environment_id = azurerm_container_app_environment.env.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "simple-hello-world"
+      image  = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+    }
+  }
+
+  ingress {
+    target_port = 80
+    external_enabled = true
+    traffic_weight {
+      percentage = 100
+      latest_revision = true
+    }
+  }
+}
---
+++ lab9c/outputs.tf
@@ -0,0 +1,3 @@
+output "app_url" {
+  value = "https://${azurerm_container_app.app.latest_revision_fqdn}"
+}
